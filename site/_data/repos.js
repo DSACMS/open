ffcd315c68d3017ff7f5502197c1c45254e1811f
Brandon Yee <6111102+Firebird1029@users.noreply.github.com>
2024-07-15 09:44:22 -1000

    fix horizontal scroll and other bugs

--- site/_data/repos.js
+++ site/_data/repos.js
@@ -126,12 +126,12 @@ const manualOverrides = {
       "A developer-friendly, standards-based API that enables Medicare beneficiaries to connect their claims data to applications, services and research programs they trust.",
     category: "apis",
   },
-  "CMSgov/easi-app": {
-    homepageUrl: "https://easi.cms.gov",
-    description:
-      "EASi is a web application supporting the IT governance process at CMS.",
-    category: "apps",
-  },
+  // "CMSgov/easi-app": {
+  //   homepageUrl: "https://easi.cms.gov",
+  //   description:
+  //     "EASi is a web application supporting the IT governance process at CMS.",
+  //   category: "apps",
+  // },
   "Enterprise-CMCS/macpro-quickstart-serverless": {
     description:
       "Template for a serverless form submission application, built and deployed to AWS with the Serverless Application Framework.",
@@ -265,7 +265,9 @@ module.exports = async () => {
     Object.keys(manualOverrides).forEach((path) => {
       const [org, repo] = path.split("/")
       const repoData = repos.find((x) => x.owner.login == org && x.name == repo)
-      Object.assign(repoData, manualOverrides[path])
+      if (repoData) {
+        Object.assign(repoData, manualOverrides[path])
+      }
     })
 
     repos.forEach((repo) => {
